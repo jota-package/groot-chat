d469481dad6962444aa1d67758f3eb65be920293
juan <jotabeles.web@gmail.com>
2021-02-10 11:51:46 -0500

    Aumentando el lexico del chat

--- index.js
+++ index.js
@@ -1,5 +1,27 @@
-function groot (mensaje){
-	return "Yo soy Groot.";
+const lista_respuestas = ["Yo soy Groot.", "Yo, soy Groot", "Yo soy. Groot", "Yo soy Groot!", "Yooo, soy groot"];
+
+function procesar_respuesta ($mensaje) {
+	let posicion = Math.floor(Math.random() * (lista_respuestas.length - 1));
+	return lista_respuestas[posicion];
+}
+
+function pedirConsejo (mensaje){
+	return procesar_respuesta($mensaje);
+}
+
+function responder (mensaje){
+	return procesar_respuesta($mensaje);
+}
+
+function cantar (mensaje){
+	let max_frases = 10;
+	let min_frases = 3;
+	let cantidad_frases = Math.floor(Math.random() * (max_frases - min_frases)) + min_frases;
+	let cancion = "";
+	for(let i = 0; i < cantidad_frases; i++){
+		cancion += procesar_respuesta("rap rap") + " ";
+	}
+	return cancion;
 }
 
 module.exports.Groot = groot;
